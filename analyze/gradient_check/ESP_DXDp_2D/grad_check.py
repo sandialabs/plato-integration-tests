03657c087b934e25f334ae6c0615ea650f33d2bc
ralberd <ralberd@sandia.gov>
2023-03-21 17:03:21 -0600

    fixing ESP DXDP grad checks by using sensitivity map from ESP

--- analyze/gradient_check/ESP_DXDp_2D/grad_check.py
+++ analyze/gradient_check/ESP_DXDp_2D/grad_check.py
@@ -2,6 +2,7 @@ import sys
 import os
 import exodus
 import PythonPlatoESP
+import numpy as np
 
 # specify sensitivity tolerance
 sensitivityTol = float(sys.argv[1])
@@ -29,6 +30,10 @@ os.system(espCommand)
 mesh = exodus.ExodusDB()
 mesh.read(model + ".exo")
 
+# read in sensitivity map
+sensMap = np.loadtxt('./ESP_Mesh/Scratch/plato/sensMap.txt', dtype=int)
+sensMap = sensMap[2:]
+
 # create plato esp instances
 platoESP0 = PythonPlatoESP.PlatoESP(performerInput, performerOperations, "plato esp instance")
 platoESP0.initialize()
@@ -51,8 +56,9 @@ def check_face_sensitivity(dim, faceCoord, grad, gold):
     coordTol = 1.0E-8 * abs(2.0*faceCoord)
 
     errors = []
-    for nodeIndex in range(mesh.numNodes):
-        X = mesh.getCoordData(nodeIndex, "index")
+    for nodeIndex in range(len(sensMap)):
+        nodeID = sensMap[nodeIndex] - 1
+        X = mesh.getCoordData(nodeID, "index")
         if (X[dim] <= (faceCoord + coordTol) and X[dim] >= (faceCoord - coordTol)):
             relError = (grad[3*nodeIndex + dim] - gold) / gold
             errors.append(abs(relError))
