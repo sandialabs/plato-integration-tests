53f3ddede76b8aedcb130234d2830c13a48d1db9
Ray Wildman <rawildm@sandia.gov>
2025-11-21 09:52:29 -0700

    Sets openmp environment variables for sierra tests

--- utilities/cmake/mpi_helpers.cmake
+++ utilities/cmake/mpi_helpers.cmake
@@ -18,3 +18,23 @@ math(EXPR NUM_PROCESSORS "${NUM_RANKS} * ${NUM_THREADS}")
 set( ${OUT_NUM_PROCESSORS} "${NUM_PROCESSORS}" PARENT_SCOPE)
 
 endfunction(total_processors)
+
+# Sets the OpenMP environment variables for a test
+function(set_openmp_test_properties)
+
+set(OPTIONS "")
+set(ONE_VALUE_ARGS TEST_NAME NUM_RANKS NUM_THREADS)
+set(MULTI_VALUE_ARGS "")
+cmake_parse_arguments(PARSE_ARGV 0 ARG "${OPTIONS}" "${ONE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}")
+
+if(NOT ARG_NUM_RANKS)
+    set(ARG_NUM_RANKS 1)
+endif()
+if(NOT ARG_NUM_THREADS)
+    set(ARG_NUM_THREADS 1)
+endif()
+
+total_processors(${ARG_NUM_RANKS} ${ARG_NUM_THREADS} NUM_PROCESSORS)
+set_tests_properties( ${ARG_TEST_NAME} PROPERTIES PROCESSORS ${NUM_PROCESSORS} ENVIRONMENT "OMP_NUM_THREADS=${ARG_NUM_THREADS};OMP_PROC_BIND=close;OMP_PLACES=threads")
+
+endfunction(set_openmp_test_properties)
